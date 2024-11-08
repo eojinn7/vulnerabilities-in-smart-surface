0fb94f18d144fc8ba0a4a8628fe2fd5570de8375
JINNY <eojinny@grinnell.edu>
2024-11-08 15:51:54 -0600

    Initial commit of agent & environment classes

---
+++ agent.java
@@ -0,0 +1,12 @@
+public class agent {
+  
+  //field to hold vote of agent
+  String vote;
+
+  //position of agent:
+  int row;
+  int col;
+
+  
+
+}
---
+++ environment.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class environment {
+
+  public static void main(String args[]) {
+    // 2d array for grid incase Java fx doesn't happen
+    String[][] env = new String[4][3]; //4 columns and 3 rows
+
+    
+
+  }
+ 
+}
