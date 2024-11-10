7a5f944752a3c03c74143f97bfdbdea19a8f9271
JINNY <eojinny@grinnell.edu>
2024-11-10 16:29:28 -0600

    initial states

--- agent.java
+++ agent.java
@@ -2,11 +2,28 @@ public class agent {
   
   //field to hold vote of agent
   String vote;
+//field to hold agent type
+  String type;
 
   //position of agent:
   int row;
   int col;
 
-  
+  //constructor
+  public agent(int x, int y, String s)
+  {
+    this.vote = "V";
+    this.row = x;
+    this.col= y;
+    this.type = s;
+  }
+
+  //getter for weight of vote
+  public int getW() {
+    if(this.type.equalsIgnoreCase("penalty")) {
+      return 0;
+    } else 
+    if(this.type.equalsIgnoreCase(""))
+  }
 
 }
