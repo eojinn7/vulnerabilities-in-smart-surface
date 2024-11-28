7161522a04d3b42d750ae46a91c7511ab6f96c96
eojinn7 <144048005+eojinn7@users.noreply.github.com>
2024-11-27 18:52:17 -0600

    Update Cell.java to include defective feature

--- src/Cell.java
+++ src/Cell.java
@@ -1,20 +1,30 @@
 // Cell.java
 public class Cell {
-    private int row;
-    private int col;
-    private String symbol;
+  private int row;
+  private int col;
+  private String symbol;
+  private boolean defective;
 
-    public Cell(int row, int col) {
-        this.row = row;
-        this.col = col;
-        this.symbol = "."; // default symbol, has no decision
-    }
+  public Cell(int row, int col) {
+    this.row = row;
+    this.col = col;
+    this.symbol = "."; // default symbol, has no decision
+    this.defective = false; // default state is false
+  }
 
-    public String getSymbol() {
-        return symbol;
-    }
+  public String getSymbol() {
+    return symbol;
+  }
 
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
+  public void setSymbol(String symbol) {
+    this.symbol = symbol;
+  }
+
+  public boolean isDefective() {
+    return defective;
+  }
+
+  public void setDefective(boolean defective) {
+    this.defective = defective;
+  }
 }
